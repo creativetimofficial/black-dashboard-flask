cffe140ec4e7d6fe5a4952a7d17274640abce165
Andrew Piispanen <apiispanen1@babson.edu>
2023-04-02 20:10:50 -0400

    stupidtinyMCE

--- apps/static/assets/js/speeches.js
+++ apps/static/assets/js/speeches.js
@@ -14,7 +14,7 @@ return randomValue
 
 tinymce.init({
   selector: '#form3',
-  plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed linkchecker a11ychecker tinymcespellchecker permanentpen powerpaste advtable advcode editimage tinycomments tableofcontents footnotes mergetags autocorrect typography inlinecss',
+
   toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
   tinycomments_mode: 'embedded',
   tinycomments_author: 'Author name',
